34f517740fcb4fa5023169a35ffbd997f132e400
Rahul Roy <rahulroy6918@gmail.com>
2021-10-23 13:54:02 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -5,15 +5,15 @@ def get_prices():
     crypto_coins = ["BTC", "ETH", "XRP", "LTC", "BCH", "ADA", "DOT", "LINK", "BNB", "XLM"]
 
     get_crypto_data = requests.get(
-        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms=USD,EUR,IND".format(",".join(crypto_coins))).json()["RAW"]
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms=USD".format(",".join(crypto_coins))).json()["RAW"]
 
     data = {}
     for i in get_crypto_data:
         data[i] = {
             "coin": i,
-            "price": get_crypto_data[i]["USD"]["EUR"]["IND"]["PRICE"],
-            "change_day": get_crypto_data[i]["USD"]["EUR"]["IND"]["CHANGEPCT24HOUR"],
-            "change_hour": get_crypto_data[i]["USD"]["EUR"]["IND"]["CHANGEPCTHOUR"]
+            "price": get_crypto_data[i]["USD"]["PRICE"],
+            "change_day": get_crypto_data[i]["USD"]["CHANGEPCT24HOUR"],
+            "change_hour": get_crypto_data[i]["USD"]["CHANGEPCTHOUR"]
         }
 
     return data
